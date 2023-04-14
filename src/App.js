41655581504aea6328bd6511a16336add3ffc7f8
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-04-14 15:42:03 +0200

    add delete func

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import Header from './components/Header';
+import Content from './components/Content';
 
-
+import { useState } from 'react';
 
 function App() {
+
+const [ items, setItems ] = useState([
+  { id: 1, checked: true, item: 'push ups' },
+  { id: 2, checked: false, item: 'squats' },
+  { id: 3, checked: false, item: 'sit ups' }
+])
+
+const handleCheck = (id) => {
+  const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked }: item )
+  setItems(listItems)
+}
+
+const handleDelete = (id) => {
+  const listItems = items.filter((item) => item.id !== id)
+  setItems(listItems)
+}
+
   return (
     <div className="App">
       <Header />
+      <Content 
+      items={items} 
+      setItems={setItems} 
+      handleCheck={handleCheck} 
+      handleDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
 
-const Content = () => {
+const Content = ({ items, setItems, handleCheck, handleDelete }) => {
   return (
-    <div>Content</div>
+    <ul>{items.map((item) => (
+        <li className='item' key={item.id}>
+            <input 
+            type='checkbox' 
+            checked={item.checked} 
+            onChange={() => handleCheck(item.id) } 
+            />     
+            {item.item}
+            <FaTrashAlt 
+            role='button' 
+            onClick={() => handleDelete(item.id)}
+            tabIndex={0}
+            />
+            </li>
+    ))} </ul>
   )
 }
 
